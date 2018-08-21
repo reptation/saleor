ffa64f31dc6f6d9d53bc968344fc5dddb670cb87
Artur Smęt <artur@mirumee.com>
2018-08-21 13:46:36 +0200

    Add further payment API improvements

--- saleor/graphql/api.py
+++ saleor/graphql/api.py
@@ -43,6 +43,9 @@ from .page.mutations import PageCreate, PageDelete, PageUpdate
 from .product.schema import ProductMutations, ProductQueries
 from .payment.types import Payment, PaymentGatewayEnum
 from .payment.resolvers import resolve_payments, resolve_payment_client_token
+from .payment.mutations import (
+    CheckoutPaymentMethodCreate, PaymentMethodCharge, PaymentMethodRefund,
+    PaymentMethodVoid)
 from .shipping.resolvers import resolve_shipping_zones
 from .shipping.types import ShippingZone
 from .shipping.mutations import (
