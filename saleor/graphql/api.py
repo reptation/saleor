6ce27457ecbcddac61bde14e08dc666f91be49f0
Artur Smęt <artur@mirumee.com>
2018-08-22 16:51:47 +0200

    Improve code style

--- saleor/graphql/api.py
+++ saleor/graphql/api.py
@@ -57,7 +57,7 @@ from .checkout.mutations import (
     CheckoutCreate, CheckoutLinesAdd, CheckoutLinesUpdate, CheckoutLineDelete,
     CheckoutCustomerAttach, CheckoutCustomerDetach,
     CheckoutShippingAddressUpdate, CheckoutEmailUpdate, CheckoutComplete)
-from .checkout.resolvers import resolve_checkouts
+from .checkout.resolvers import resolve_checkouts, resolve_checkout_lines
 from .shop.types import Shop
 from .shop.mutations import (
     AuthorizationKeyAdd, AuthorizationKeyDelete, ShopDomainUpdate,
@@ -151,8 +151,11 @@ class Query(ProductQueries):
         query=graphene.String(description=DESCRIPTIONS['user']))
     node = graphene.Node.Field()
 
-    def resolve_cart_line(self, info, id):
+    def resolve_checkout_line(self, info, id):
         return get_node(info, id, only_type=CheckoutLine)
+
+    def resolve_checkout_lines(self, info, query=None, **kwargs):
+        return resolve_checkout_lines(info, query)
 
     def resolve_checkouts(self, info, query=None, **kwargs):
         resolve_checkouts(info, query)
