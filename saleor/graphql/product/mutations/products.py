dd9b1c57b10eb0fd33759cbe46548d3d385cac87
oldPadavan <evggoncharov@gmail.com>
2018-09-05 10:50:31 +0300

    Rename input class

--- saleor/graphql/product/mutations/products.py
+++ saleor/graphql/product/mutations/products.py
@@ -214,7 +214,7 @@ class AttributeValueInput(InputObjectType):
         required=True, description='Value of an attribute.')
 
 
-class ProductPropertiesInput(graphene.InputObjectType):
+class ProductInput(graphene.InputObjectType):
     attributes = graphene.List(
         AttributeValueInput,
         description='List of attributes.')
@@ -293,7 +293,7 @@ class ProductUpdate(ProductCreate):
     class Arguments:
         id = graphene.ID(
             required=True, description='ID of a product to update.')
-        input = ProductPropertiesInput(
+        input = ProductInput(
             required=True, description='Fields required to update a product.')
 
     class Meta:
