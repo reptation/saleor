1082182f305a3f257542291c71057a3a3218001a
Pacu2 <filip.m.kucharczyk@gmail.com>
2018-10-21 14:38:46 +0200

    Add documentation on adding new payment gateway, fix style and inconsistencies in naming

--- saleor/graphql/api.py
+++ saleor/graphql/api.py
@@ -42,7 +42,7 @@ from .page.types import Page
 from .page.mutations import PageCreate, PageDelete, PageUpdate
 from .product.schema import ProductMutations, ProductQueries
 from .payment.types import Payment, PaymentGatewayEnum
-from .payment.resolvers import resolve_payments, resolve_payment_client_token
+from .payment.resolvers import resolve_payments, resolve_payment_transaction_token
 from .payment.mutations import (
     CheckoutPaymentCreate, PaymentCapture, PaymentRefund,
     PaymentVoid)
@@ -121,7 +121,7 @@ class Query(ProductQueries):
             description=DESCRIPTIONS['page']),
         description='List of the shop\'s pages.')
     payment = graphene.Field(Payment, id=graphene.Argument(graphene.ID))
-    payment_client_token = graphene.Field(
+    payment_transaction_token = graphene.Field(
         graphene.String, args={'gateway': PaymentGatewayEnum()})
     payments = DjangoFilterConnectionField(
         Payment,
@@ -215,8 +215,8 @@ class Query(ProductQueries):
     def resolve_payment(self, info, id):
         return graphene.Node.get_node_from_global_id(info, id, Payment)
 
-    def resolve_payment_client_token(self, info, gateway=None):
-        return resolve_payment_client_token(gateway)
+    def resolve_payment_transaction_token(self, info, gateway=None):
+        return resolve_payment_transaction_token(gateway)
 
     @permission_required('order.manage_orders')
     def resolve_payments(self, info, query=None, **kwargs):
