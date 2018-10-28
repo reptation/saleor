8a71e3ca6a9ca61d568c5c9dd09b7bdfbcded4e1
reptation <david@musicalquilt.com>
2018-10-28 05:18:30 +0000

    remove duplicate script

--- docker-dev-environment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker-compose build
-docker-compose run web python3 manage.py migrate
-#docker-compose run web python3 manage.py collectstatic
-docker-compose run web python3 manage.py createsuperuser
-docker-compose up
